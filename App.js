8674040cce9b70af96fb31cfb7e4edd0820ee759
Yashu <yashu004unique@gmail.com>
2025-12-16 10:12:21 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,33 @@
+        // const heading = React.createElement("h1", {}, "Hello World using React...")
+        // const root = ReactDOM.createRoot(document.getElementById("root"))
+        // root.render(heading)
+
+
+
+
+        // creating nested divs in html using react --- 3 arguments---- tagname, {id & attributes}, wt we put inside the tag(children)
+
+        // const parent = React.createElement(
+        //         "div", 
+        //         {id: "parent"}, 
+        //         React.createElement(
+        //                 "div",
+        //                 {id: "child"}, 
+        //                 React.createElement("h1",{}, "I m a h1 tag")))
+        // const root = ReactDOM.createRoot(document.getElementById("root"))
+        // root.render(parent)
+
+
+        const parent = React.createElement(
+                "div", 
+                {id: "parent"}, 
+                React.createElement(
+                        "div",
+                        {id: "child"}, [ 
+                        React.createElement("h1",{}, "I m a h1 tag"),
+                        React.createElement("h2", {}, "I m a h2 tag")
+                        ]
+                )
+        )
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(parent)
