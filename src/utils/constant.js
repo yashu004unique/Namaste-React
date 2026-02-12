f061338c7e99695a0d5008df28e09f0fdb10f9a4
yashu004unique <yashu004unique@gmail.com>
2026-02-12 21:12:19 +0530

    Fix API URLs to relative paths

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,6 +4,6 @@ export const LOGO_URL =
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_URL = "http://localhost:5000/menu/";
+export const MENU_URL = "/menu/";
 
-export const RESTAURANT_URL = "http://localhost:5000/restaurants";
+export const RESTAURANT_URL = "/restaurants";
