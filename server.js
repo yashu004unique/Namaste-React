8e288e1de9e12cd20a26e122927d13fb4d11d1be
yashu004unique <yashu004unique@gmail.com>
2026-02-12 20:24:59 +0530

    changed app.get to app.use to avoid the error

--- server.js
+++ server.js
@@ -641,7 +641,7 @@ app.get("/menu/:id", (req, res) => {
 
 // Serve React build
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
